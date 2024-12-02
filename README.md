fcce42207351ef4c052492ac10c513fc1252fe47
Jeffrey Yoo Warren <jeff@unterbahn.com>
2024-12-02 10:54:48 -0500

    comments export

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # publiclab-archive
 A static archive of the PublicLab.org site in markdown files
+
+See https://github.com/publiclab/publiclab-archive/blob/main/wiki/topics.md
---
+++ script/tag-export.rb
@@ -0,0 +1,20 @@
+require 'fileutils'
+
+tags = Tag.joins(:node_tag, :node)
+          .select('MAX(node.nid), node.status, term_data.*, community_tags.*')
+          .where('node.status = ?', 1)
+          .where('community_tags.date > ?', (DateTime.now - 1.month).to_i)
+          .group(:name)
+
+tags.each do |tag|
+  name = tag.name.sub('/','-')
+  path = 'public/static/tag/' + name + ".md"
+  nodes = tag.nodes
+  text = ""
+  nodes.each do |node|
+    text += "[#{node.title}](#{node.path})"
+  end
+  File.open(path, 'w') do |file|
+    file.write(text)
+  end
+end
