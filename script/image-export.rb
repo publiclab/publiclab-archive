92db2461c90f99704c3646dc449c0d3348ef4549
Jeffrey Yoo Warren <jeff@unterbahn.com>
2024-12-26 20:47:45 -0500

    node shortlink export

---
+++ script/image-export.rb
@@ -0,0 +1,18 @@
+require 'fileutils'
+
+images = Image.all
+
+images.each do |image|
+  if image.photo_file_name
+    path = "public/static/i/#{image.id}.md"
+    text = "---\n"
+    text += "id: #{image.id}\n"
+    text += "path: #{image.path}\n"
+    text += "created_at: #{image.created_at.to_s(:long)}\n"
+    text += "---\n\n"
+    text += "<script>window.location.replace('.." + image.path + '");</script>';
+    File.open(path, 'w') do |file|
+      file.write(text)
+    end
+  end
+end
